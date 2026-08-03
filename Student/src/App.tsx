4e94584cf0c66a17129643a57326644c8a11bf76
sindhubavana <sindhubavana.gsb@gmail.com>
2026-08-03 16:06:48 +0530

    Update App.tsx

--- Student/src/App.tsx
+++ Student/src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { PaymentProvider } from './context/PaymentContext';
 import { AppRouter } from './router/AppRouter';
-import { useEffect } from 'react';
-import { initSocket } from './lib/socket';
+// import { useEffect } from 'react';
+// import { initSocket } from './lib/socket';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'; // keeps Vite structure intact
 
